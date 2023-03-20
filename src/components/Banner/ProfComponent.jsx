bab6d5f8cd29fd0cf3226e9661b6ca86ac2b12cc
Pedro Robalo <pedro.robalo.ext@siemens.com>
2023-03-20 00:30:25 +0000

    Firts Commit - Started Portfolio Creation

---
+++ src/components/Banner/ProfComponent.jsx
@@ -0,0 +1,184 @@
+import React from "react";
+import styled from "styled-components";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { Slide } from "react-awesome-reveal";
+
+const ProfComponent = () => {
+  return (
+    <Container id="home">
+      <Slide direction="left">
+        <Texts>
+          <h4>
+            Hello <span className="blue">I' am</span>
+          </h4>
+          <h1 className="blue">Pedro Robalo</h1>
+          <h3>Software Engineer</h3>
+          <p>
+            Some say I am Developer, some say I am a Programmer, and some say I am an Engineer. But what I am mostly is a person driven by curiosity and all tech stuff.
+          </p>
+          <button><a href="#footer">Let's talk</a></button>
+          <Social>
+            <p>Check out my</p>
+            <div className="social-icons">
+              <span>
+                <a href="https://github.com/PedroRobalo1994">
+                  <AiOutlineGithub />
+                </a>
+              </span>
+              <span>
+                <a href="https://www.linkedin.com/in/pedro-robalo-42a39212a/">
+                  <FaLinkedinIn />
+                </a>
+              </span>
+              <span>
+                <a href="https://www.instagram.com/pedro_robalo94/">
+                  <AiOutlineInstagram />
+                </a>
+              </span>
+            </div>
+          </Social>
+        </Texts>
+      </Slide>
+      <Slide direction="right">
+        <Profile>
+          <img
+            src={require("../../images/profile-pic.png")}
+            alt="profile"
+          />
+        </Profile>
+      </Slide>
+    </Container>
+  );
+};
+
+export default ProfComponent;
+
+const Container = styled.div`
+  display: flex;
+  gap: 2rem;
+  padding-top: 3rem;
+  width: 80%;
+  max-width: 1280px;
+  margin: 0 auto;
+  z-index: 1;
+  @media (max-width: 840px) {
+    width: 90%;
+  }
+
+  @media (max-width: 640px) {
+    flex-direction: column;
+  }
+`;
+const Texts = styled.div`
+  flex: 1;
+  h4 {
+    padding: 1rem 0;
+    font-weight: 500;
+  }
+  h1 {
+    font-size: 2rem;
+    font-family: "Secular One", sans-serif;
+    letter-spacing: 2px;
+  }
+  h3 {
+    font-weight: 500;
+    font-size: 1.2rem;
+    padding-bottom: 1.2rem;
+    text-transform: capitalize;
+  }
+  p {
+    font-weight: 300;
+  }
+
+  button {
+    padding: 0.7rem 2rem;
+    margin-top: 3rem;
+    cursor: pointer;
+    background-color: #01b1be;
+    border: none;
+    color: #fff;
+    font-weight: 500;
+    filter: drop-shadow(0px 10px 10px #01b1bf);
+    :hover {
+      filter: drop-shadow(0px 10px 10px #01b1cf);
+    }
+    a{
+      color: #fff;
+      text-decoration: none;
+      font-weight: 400;
+      position: relative;
+      :before{
+          content: "";
+          position: absolute;
+          left: 0;
+          right: 0;
+          bottom: -5px;
+          height: 2px;
+          background-color: #fff;
+          transform: scale(0);
+          transform-origin: right;
+          transition: transform 400ms ease-in-out;
+      }
+  }
+`;
+const Social = styled.div`
+  margin-top: 3rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  p {
+    font-size: 0.9rem;
+    @media (max-width: 690px) {
+      font-size: 0.7rem;
+    }
+  }
+
+  .social-icons {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    span {
+      width: 2.3rem;
+      height: 2rem;
+      clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+      background-color: #01b1be;
+      position: relative;
+      transition: transform 400ms ease-in-out;
+      :hover {
+        transform: rotate(360deg);
+      }
+    }
+
+    a {
+      color: #fff;
+      position: absolute;
+      top: 55%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+`;
+const Profile = styled.div`
+  img {
+    width: 25rem;
+    filter: drop-shadow(0px 10px 10px #01b1be);
+    transition: transform 400ms ease-in-out;
+    @media (max-width: 790px) {
+      width: 20rem;
+    }
+
+    @media (max-width: 660px) {
+      width: 18rem;
+    }
+
+    @media (max-width: 640px) {
+      width: 100%;
+    }
+  }
+
+  :hover img {
+    transform: translateY(-10px);
+  }
+`;
---
+++ src/components/Service/Services.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { AiFillCloud } from "react-icons/ai";
+import { MdDesignServices } from "react-icons/md";
+import { CgWebsite } from "react-icons/cg";
+import styled from "styled-components";
+import Card from "./Card";
+import { Slide } from "react-awesome-reveal";
+
+const Services = () => {
+  return (
+    <Container id="service">
+      <Slide direction="down">
+        <h4>
+          <span className="green">My Services</span>
+        </h4>
+        <h1>What I Do</h1>
+      </Slide>
+      <Cards>
+        <Slide direction="left">
+          <Card
+            Icon={MdDesignServices}
+            title={"QA Automation Engineer"}
+          />
+        </Slide>
+        <Slide direction="up">
+          <Card
+            Icon={AiFillCloud}
+            title={"DevOps Engineer"}
+          />
+        </Slide>
+        <Slide direction="right">
+          <Card
+            Icon={CgWebsite}
+            title={"Web Developer"}
+          />
+        </Slide>
+      </Cards>
+    </Container>
+  );
+};
+
+export default Services;
+
+const Container = styled.div`
+  width: 80%;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 3rem 0;
+  @media (max-width: 840px) {
+    width: 90%;
+  }
+
+  h1 {
+    padding-top: 1rem;
+  }
+`;
+const Cards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  margin-top: 4rem;
+  gap: 1rem;
+`;
